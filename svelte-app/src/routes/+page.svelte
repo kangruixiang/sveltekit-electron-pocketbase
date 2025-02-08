de6cd8fb58b0a683832fd87706e7e58e525c5b21
Kang <thisispiggy@gmail.com>
2025-02-08 12:57:41 -0500

    updated test note

--- svelte-app/src/routes/+page.svelte
+++ svelte-app/src/routes/+page.svelte
@@ -1,10 +1,16 @@
 <script lang="ts">
 	let { data } = $props();
 
-	const testNote = data.notes[0].title;
+	const notes = data.notes;
 </script>
 
 <h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<p>
+	Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation.
+</p>
 
-<p>Test Note from Pocketbase: {testNote}</p>
+{#if notes.length > 0}
+	<p>Test Note from Pocketbase: {notes[0].title}</p>
+{:else}
+	<p>No notes found.</p>
+{/if}
